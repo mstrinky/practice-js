5d8f1f6fcfcdae1a29ce79482913ef1ea83f9b42
Mst Rinky Akter <mstrinkya@gmail.com>
2023-02-05 00:00:59 +0600

    continue loop

--- loop.js
+++ loop.js
@@ -1,6 +1,16 @@
 var loop = 1;
 while(loop<40){
     console.log('ajke amar mon valo nei');
-    
+    console.log(loop);
     loop ++;
+}
+
+// show odd numbers 
+for(var number = 581; number<=623; number +=2){
+    console.log(number);
+}
+
+// show even numbers 
+for(var number = 412; number<=456; number +=2){
+    console.log(number);
 }
