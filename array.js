0e0a424642b04e195925d552032499d8cd149d54
Mst Rinky Akter <mstrinkya@gmail.com>
2023-02-04 23:12:15 +0600

    array close

--- array.js
+++ array.js
@@ -1,3 +1,21 @@
 // how to declare a array 
 var arrayList = ['rinky','jesmin','salu'];
-
+// number of array 
+var numberOfArray = arrayList.length;
+console.log(numberOfArray);
+// mone kori 2nom index a ki ase ta korar niom 
+console.log(arrayList[2]);
+// mone kori jesmin koto nom index a ase ta ber kori 
+console.log(arrayList.indexOf('jesmin'));
+// add element in array 
+arrayList.push('bithi');
+console.log(arrayList);
+// remove an element in array 
+arrayList.pop();
+console.log(arrayList);
+// add an element of the first position in array 
+arrayList.unshift('sorno');
+console.log(arrayList);
+// remove the first element of array 
+arrayList.shift();
+console.log(arrayList);
