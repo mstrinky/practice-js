f98c54bb65e16d4fd0d81be043aa0e7275728fb3
Mst Rinky Akter <mstrinkya@gmail.com>
2023-02-04 21:22:23 +0600

    variable close

--- variable.js
+++ variable.js
@@ -1,1 +1,27 @@
+// declare a variable
 var name = 'rinky';
+
+// add 
+var num1 = 5;
+var num2 = 3;
+var total = num1 + num2;
+console.log(total);
+
+// minus 
+minus = num1 - num2;
+console.log(minus);
+
+// multiplication 
+var multiply = num1*num2;
+console.log(multiply);
+
+// divided and parsefloat
+var divided = num1/num2;
+var number = parseInt(divided);
+console.log(number);
+
+// toFixed 
+// * dosomik er por joto songkha nite chaw toFixed e to likhte hobe , r jodi toFixed likhar por ()faka rakho thle akta purnosongkha dekhabe 
+var floatNumber = 4.5629;
+var number = floatNumber.toFixed(2);
+console.log(number);
